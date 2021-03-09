f9fd2f971a3339092b12e5301423847cd4907e74
Caleb Kang <caleb@determined.ai>
2021-03-09 13:15:25 -0700

    fix: fix jest for typescript

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/*
+ * Workaround for typescript Jest and react-testing-library.
+ * https://stackoverflow.com/a/64872224
+ */
+global.matchMedia = global.matchMedia || function matchMedia() {
+  return {
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+};
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/people/i);
   expect(linkElement).toBeInTheDocument();
 });
