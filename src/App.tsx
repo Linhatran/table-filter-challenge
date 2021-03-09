59de86fd8a2e59826c7d0c9b4936f963c2f35d59
Caleb Kang <caleb@determined.ai>
2021-03-09 10:20:59 -0700

    chore: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import { Col, Input, Row, Table } from "antd";
+import "./App.css";
+
+import columns from "./columns";
+import { getPeopleList, Person } from "./api";
+
+/* TODO: Implement debouncer */
+function debounce(func: () => void, wait: number, immediate = true) {
+}
 
 function App() {
+  const [data, setData] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /* TODO: Implement search filter */
+
+  const fetchPeople = useCallback(() => {
+    setIsLoading(true);
+    getPeopleList()
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  useEffect(() => {
+    fetchPeople();
+  }, [fetchPeople]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People</h1>
+      <Row className="Row">
+        <Col>
+          <Input allowClear placeholder="search people" />
+        </Col>
+      </Row>
+      <Table
+        bordered={true}
+        columns={columns}
+        dataSource={data}
+        loading={isLoading}
+      />
     </div>
   );
 }
+
 
 export default App;
